d857bc3a1f267b5762b9d0521b3f2674d0af7063
rajgopalhota <rajgopalhota@gmail.com>
2023-09-20 23:06:45 +0530

    Nav

---
+++ vaayu/src/components/Navbar.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import logo from './../assets/logo.png'
+
+export default function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg custom-navbar">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          <img src={logo} className="navbar-image"/>
+        </a>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Link
+              </a>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Dropdown
+              </a>
+              <ul className="dropdown-menu">
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Action
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Another action
+                  </a>
+                </li>
+                <li>
+                  <hr className="dropdown-divider" />
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Something else here
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link disabled" aria-disabled="true">
+                Disabled
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
