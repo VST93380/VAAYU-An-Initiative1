8d8f93f83d5a7af8f60780e1797269d70e5d1a92
Raja-25 <rajarajendra925@gmail.com>
2023-09-22 17:21:57 +0530

    created registration model and completed database connection

--- server/server.js
+++ server/server.js
@@ -1,12 +1,29 @@
 import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 
-app.get("/", (req, res) => {
-  res.send("connected");
-});
+// Use async/await to connect to the database
+(async () => {
+  try {
+    await mongoose.connect(process.env.DATABASE_ACCESS, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Database Connected!!");
+  } catch (error) {
+    console.error("Error connecting to the database:", error);
+  }
+})();
+
+app.use(bodyparser.json())
+app.use(express.json());
+app.use(cors());
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log("Server listening the port http://localhost/" + port);
+  console.log(`Server listening on http://localhost:${port}`);
 });
