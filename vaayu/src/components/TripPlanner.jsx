ccfebd2c600ff866ba4485727e3984eaff3e6e1d
rajgopalhota <rajgopalhota@gmail.com>
2023-09-25 19:46:20 +0530

    cg

--- vaayu/src/components/TripPlanner.jsx
+++ vaayu/src/components/TripPlanner.jsx
@@ -11,7 +11,8 @@ function ItineraryForm() {
   };
 
   return (
-    <div className="container">
+    <div className="container mt-5 tripplanner">
+      <h2>Plan Itinerary</h2>
       <div className="glass p-4">
         <form onSubmit={handleSubmit}>
           <div className="mb-3">
@@ -44,33 +45,37 @@ function ItineraryItem({ title, details }) {
   const [isDone, setIsDone] = useState(false);
 
   return (
-    <div className="card mb-3">
-      <div className="card-header">
-        <div className="form-check">
-          <input
-            type="checkbox"
-            className="form-check-input"
-            checked={isDone}
-            onChange={() => setIsDone(!isDone)}
-          />
-          <label className="form-check-label" htmlFor="doneCheck">
-            {title}
-          </label>
+    <div className="col-lg-4 col-md-6 mb-4 d-flex">
+      <div className="card flex-fill">
+        <div className="card-header">
+          <div className="form-check">
+            <input
+              type="checkbox"
+              className="form-check-input"
+              checked={isDone}
+              onChange={() => setIsDone(!isDone)}
+            />
+            <label className="form-check-label" htmlFor="doneCheck">
+              {title}
+            </label>
+          </div>
         </div>
-      </div>
-      <div className="card-body">
-        <p className="card-text">{details}</p>
+        <div className="card-body">
+          <p className="card-text">{details}</p>
+        </div>
       </div>
     </div>
   );
 }
 
 function TripPlanner() {
+  // Replace this with your actual data
   const itineraryData = [
     { title: "Place to visit 1", details: "Details about the place." },
     { title: "Place to visit 1", details: "Details about the place." },
     { title: "Place to visit 1", details: "Details about the place." },
     { title: "Place to visit 1", details: "Details about the place." },
+    // Add more itinerary items here
   ];
 
   return (
@@ -78,13 +83,15 @@ function TripPlanner() {
       <ItineraryForm />
       <div className="container">
         <h2 className="my-3">Your Itinerary</h2>
-        {itineraryData.map((item, index) => (
-          <ItineraryItem
-            key={index}
-            title={item.title}
-            details={item.details}
-          />
-        ))}
+        <div className="row">
+          {itineraryData.map((item, index) => (
+            <ItineraryItem
+              key={index}
+              title={item.title}
+              details={item.details}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
