41b9d533deeb9fc7fc16ea1b62a68edbe843d579
rajgopalhota <rajgopalhota@gmail.com>
2023-09-25 22:26:01 +0530

    cg

--- vaayu/src/components/Backvid.jsx
+++ vaayu/src/components/Backvid.jsx
@@ -1,10 +1,4 @@
-import React, { useState, useEffect } from "react";
-import b1 from "./../assets/vids/1.mp4";
-import b2 from "./../assets/vids/2.mp4";
-import b3 from "./../assets/vids/3.mp4";
-import b4 from "./../assets/vids/4.mp4";
-import b5 from "./../assets/vids/5.mp4";
-import b6 from "./../assets/vids/6.mp4";
+import bgVid from "./../assets/vids/1.mp4";
 
 export default function Backvid() {
 
@@ -12,7 +6,7 @@ export default function Backvid() {
     <div>
       <div className="hmvid">
         <video autoPlay muted loop>
-          <source src={b5} type="video/mp4" />
+          <source src={bgVid} type="video/mp4" />
           Your browser does not support the video tag.
         </video>
       </div>
