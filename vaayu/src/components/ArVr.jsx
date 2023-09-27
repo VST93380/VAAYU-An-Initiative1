69d3c316e505f45a5bfa8dc5191e94191f15287c
rajgopalhota <rajgopalhota@gmail.com>
2023-09-27 11:48:55 +0530

    cg

--- vaayu/src/components/ArVr.jsx
+++ vaayu/src/components/ArVr.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import srcData from './Json/Vrs.json'; // Make sure the path is correct
+import React from "react";
+import srcData from "./Json/Vrs.json"; // Make sure the path is correct
 
 const ArVr = () => {
   return (
-    <div>
+    <div className="vrcontainer">
       {srcData.map((src, index) => (
         <div id="iframeContainer" key={index}>
           <iframe
-            className="embeddedIframe"
-            src={src}
+            title="VRPlaces"
+            className="embeddedIframe hide-scrollbars"
+            src={src.url}
             frameBorder="0"
             allowFullScreen
             scrolling="no"
