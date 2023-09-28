37368bff1d683f84130d0ec1c45929471be8267f
rajgopalhota <rajgopalhota@gmail.com>
2023-09-28 13:13:44 +0530

    cg

--- vaayu/src/components/Weather.jsx
+++ vaayu/src/components/Weather.jsx
@@ -77,11 +77,11 @@ function WeatherComponent(props) {
                             <span className="left-front"></span>
                             <span className="right-front"></span>
                         </div>
-                        <span className="sun sunshine"></span>
-                        <span className="sun"></span>
+                        <span className="sunshine"></span>
+                        <span className="sun floating"></span>
                         <div className="cloud back">
                             <span className="left-back"></span>
-                            <span className="right-back"></span>
+                            <span className="right-back floatingLR"></span>
                         </div>
                     </div>
 
