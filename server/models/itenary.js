1bd29442a0e1cb7808568d3f1946dbaef318a5f7
Raja-25 <rajarajendra925@gmail.com>
2023-10-09 23:18:39 +0530

    crcted date in itenary

--- server/models/itenary.js
+++ server/models/itenary.js
@@ -25,8 +25,19 @@ const itinerarySchema = new mongoose.Schema({
     default: false,
   },
   dateAdded: {
-    type: Date,
-    default: Date.now,
+    type: String,
+    default:  
+    function () {
+      const today = new Date();
+      const dd = String(today.getDate()).padStart(2, "0");
+      const mm = String(today.getMonth() + 1).padStart(2, "0");
+      const yyyy = today.getFullYear();
+      const hoursIST = today.getHours();
+      const minutesIST = today.getMinutes();
+
+      return `${dd}-${mm}-${yyyy} at ${hoursIST}:${minutesIST}`;
+  }
+  
   },
 });
 
