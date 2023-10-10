1251dfdf330d8249e0d488f673a505d366a5b625
rajgopalhota <rajgopalhota@gmail.com>
2023-10-10 14:25:29 +0530

    vg

--- vaayu/src/UrlHelper.jsx
+++ vaayu/src/UrlHelper.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: "http://localhost:5000",
+  // baseURL: window.location.origin,
 });
-
+console.log(instance)
 export default instance;
